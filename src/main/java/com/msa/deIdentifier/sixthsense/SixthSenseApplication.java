acce7fa92e132943067f555cacb2329ddf48a273
griffin <griffindouble@gmail.com>
2020-06-01 14:20:26 +0900

    Initial commit

---
+++ src/main/java/com/msa/deIdentifier/sixthsense/SixthSenseApplication.java
@@ -0,0 +1,14 @@
+package com.msa.deIdentifier.sixthsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SixthSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixthSenseApplication.class, args);
+	}
+
+}
